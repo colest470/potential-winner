0143ca3b511a998004a402649e3a95042b6eb58c
Mark Tom <markbironga@gmail.com>
2026-07-09 02:23:09 -0500

    paths

--- src/edge.jl
+++ src/edge.jl
@@ -6,21 +6,21 @@
  using Images
  using Colors
 
- function detectEdges(file_path)
-     image = load(file_path)
-    
-     result = fill(RGB(0, 0, 0), size(image))
+function detectEdges(file_path)
+    image = load(file_path)
 
-     for y in axes(image, 1)
-         for x in axes(image, 2)
-             p = image[y, x]
-             if red(p) > 0.4 && green(p) > 0.4 && blue(p) > 0.3
-                 result[y, x] = RGB(0, 0, 0)
-             else
-                 result[y, x] = RGB(1, 1, 1)
-             end
-         end
-     end
+    result = fill(RGB(0, 0, 0), size(image))
 
-     return channelview(result)
-  end
+    for y in axes(image, 1)
+        for x in axes(image, 2)
+            p = image[y, x]
+            if red(p) > 0.4 && green(p) > 0.4 && blue(p) > 0.3
+                result[y, x] = RGB(0, 0, 0)
+            else
+                result[y, x] = RGB(1, 1, 1)
+            end
+        end
+    end
+
+    return channelview(result)
+end
